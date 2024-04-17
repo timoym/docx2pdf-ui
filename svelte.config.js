a7d76cc67f3e4c290ef964f1c3934e05ed7ba5de
Timo Müller <76396823+timoym@users.noreply.github.com>
2024-04-17 22:53:04 +0200

    switch to azure adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from 'svelte-adapter-azure-swa';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,19 +11,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		}),
-
-		prerender: {
-			handleHttpError: ({ path, referrer, message}) => {
-				console.error(`Failed to prerender ${path} from ${referrer}: ${message}`);
-			}
-		},
+		adapter: adapter()
 	}
 };
 
