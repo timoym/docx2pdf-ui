877cc21c66edc6a6af0485b59439e4d7bd549e85
Timo Müller <76396823+timoym@users.noreply.github.com>
2024-05-24 02:37:30 +0200

    partially implement upload backend

--- src/routes/functions-api/v1/upload/+server.ts
+++ src/routes/functions-api/v1/upload/+server.ts
@@ -1,18 +1,22 @@
-import { v4 as uuidv4 } from 'uuid';
 import { json, type RequestHandler } from '@sveltejs/kit';
+import { UPLOAD_API_KEY, UPLOAD_API_URL } from '$env/static/private';
+
+const uploadApiUrl = `${UPLOAD_API_URL}?code=${UPLOAD_API_KEY}`;
 
 export const POST: RequestHandler<{
 	fileId: string;
 }> = async (event) => {
 	const data = await event.request.formData();
 	const file = data.get('file') as FormDataEntryValue;
-	const fileId = uuidv4();
+
+	// Send the file to the api
+	const response = await fetch(uploadApiUrl, {
+		method: 'POST',
+		body: file
+	});
+	const fileId = JSON.parse(await response.text()).fileId;
 
 	// Create file entry in database
-
-	// Send the file to azure blob storage
-
-	// Return the fileId
 
 	return json({ fileId }, { status: 201 });
 };
