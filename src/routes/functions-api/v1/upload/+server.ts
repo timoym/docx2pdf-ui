71b72088c533083c80fde041d95986c4076e7141
Timo Müller <76396823+timoym@users.noreply.github.com>
2024-05-25 17:49:23 +0200

    feat: Add error handling for missing upload API configuration

--- src/routes/functions-api/v1/upload/+server.ts
+++ src/routes/functions-api/v1/upload/+server.ts
@@ -6,6 +6,10 @@ const uploadApiUrl = `${UPLOAD_API_URL}?code=${UPLOAD_API_KEY}`;
 export const POST: RequestHandler<{
 	fileId: string;
 }> = async (event) => {
+	if (UPLOAD_API_KEY === undefined || UPLOAD_API_URL === undefined) {
+		return json({ error: 'Upload API not configured' }, { status: 500 });
+	}
+
 	const data = await event.request.formData();
 	const file = data.get('file') as FormDataEntryValue;
 
